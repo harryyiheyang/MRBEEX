2de3282d65c3c9cbf371cb1e6b7a95b6f130d9c7
harryiheyang <harry.y.yang@foxmail.com>
2024-12-08 00:56:53 -0500

    MRBEEX

---
+++ R/CisMRBEE_UV.R
@@ -0,0 +1,199 @@
+#' CisMRBEE_UV: Cis Univariable Mendelian Randomization using Bias-corrected Estimating Equations
+#'
+#' This function performs univariable cis-Mendelian randomization that removes weak instrument bias using Bias-corrected Estimating Equations and identifies uncorrelated horizontal pleiotropy (UHP).
+#'
+#' @param by A vector of effect estimates from the outcome GWAS.
+#' @param bX A matrix of effect estimates from the exposure GWAS.
+#' @param byse A vector of standard errors of effect estimates from the outcome GWAS.
+#' @param bXse A matrix of standard errors of effect estimates from the exposure GWAS.
+#' @param LD The LD matrix of variants.
+#' @param Rxy The correlation matrix of estimation errors of exposures and outcome GWAS. The last column corresponds to the outcome.
+#' @param reliability.thres A threshold for the minimum value of the reliability ratio. If the original reliability ratio is less than this threshold, only part of the estimation error is removed so that the working reliability ratio equals this threshold.
+#' @param var.inf An indicator of whether using the formula of infinitesimal effect to estimate the standard error. Defaults to \code{T}.
+#' @param xQTL.max.L The maximum number of L in estimating the xQTL effects. Defaults to 10.
+#' @param xQTL.pip.thres The minimum empirical posterior inclustion probability (PIP) used in calibrating causality for xQTL data.
+#' @param xQTL.N The sample sizes of exposure.
+#' @param tauvec A vector of tuning parameters used in penalizing the direct causal effect. Default is `seq(3,10,by=1)`.
+#' @param admm.rho A parameter set in the ADMM algorithm. Default is 2.
+#' @param max.iter The maximum number of iterations for the ADMM algorithm. Default is 15.
+#' @param max.eps The convergence tolerance for the ADMM algorithm. Default is 0.005.
+#' @param ebic.gamma The extended BIC factor for model selection. Default is 2.
+#' @param use.susie n indicator of whether using SuSiE with L=1 to remove causal effect in each detection. Defaults to \code{T}.
+#' @param causal.pip.thres When use.susie=T, the PIP threshold to calibrate the removal of causal effect. Defaults to \code{0.3}.
+#'
+#' @return A list containing:
+#' \describe{
+#' \item{\code{theta}}{The estimated effect size of the tissue-gene pair.}
+#' \item{\code{gamma}}{The estimated effect sizes of the direct causal variants.}
+#' \item{\code{theta.cov}}{The variance of the estimated effect size `theta`.}
+#' \item{\code{theta.se}}{The standard error of the estimated effect size `theta`.}
+#' \item{\code{theta.z}}{The z-score of the estimated effect size `theta`.}
+#' \item{\code{Bic}}{The BIC values for each tuning parameter.}
+#' \item{\code{eQTL.fit}}{The SuSiE result of xQLT selection of exposure.}
+#' \item{\code{var.error}}{The variance of residuals.}
+#' \item{\code{var.error}}{The variance of infinitesimal effect.}
+#' \item{\code{causal.fit}}{The SuSiE result of causal effect calibration of exposure.}
+#' }
+#' @importFrom CppMatrix matrixInverse matrixMultiply matrixVectorMultiply matrixEigen matrixListProduct
+#' @importFrom susieR susie_suff_stat coef.susie susie susie_rss
+#' @export
+#'
+CisMRBEE_UV=function(by,bX,byse,bXse,LD,Rxy,
+                     xQTL.N,xQTL.max.L=10,xQTL.pip.thres=0.2,
+                     reliability.thres=0.9,var.inf=T,
+                     tauvec=seq(3,30,by=1.5),admm.rho=2,
+                     use.susie=T,causal.pip.thres=0.3,
+                     max.iter=100,max.eps=0.001,ebic.gamma=2
+                     ){
+m=length(by)
+Theta=solve(LD)
+bXest=bX
+bXest0=bXestse0=bX*0
+bXestse=bXestse0=c(1000,m)
+fit.susie=susie_rss(z=bX/bXse,R=LD,n=xQTL.N,L=xQTL.max.L,max_iter=1000)
+fit.susie=susie_rss(z=bX/bXse,R=LD,n=xQTL.N,L=sum(fit.susie$pip>xQTL.pip.thres)+1,max_iter=1000)
+indj=which(fit.susie$pip>=xQTL.pip.thres)
+if(length(indj)>0){
+LDj=LD[indj,indj]
+Thetaj=solve(LDj)
+bXest0[indj]=as.vector(Thetaj%*%(bX[indj]/bXse[indj]))*bXse[indj]
+Thetajj=LD*0
+Thetajj[indj,indj]=Thetaj
+bXestse0=sqrt(diag(Thetajj))*bXse
+LDjj=LD%*%Thetajj%*%LD
+bXest=as.vector(LD%*%(bXest0/bXse))*bXse
+bXestse=sqrt(diag(LDjj))*bXse
+}else{
+bXest=bX
+bXestse=bXse
+}
+r=reliability.adj.uv(bXest,bXestse,Theta=Theta,thres=reliability.thres)
+r=c(r,1)
+Rxy=t(t(Rxy)*r)*r
+
+by=by/byse
+byseinv=1/byse
+bX=bX*byseinv
+bXse=bXse*byseinv
+bXest=bXest*byseinv
+bXestse=bXestse*byseinv
+bXest0=bXest0*byseinv
+bXestse0=bXestse0*byseinv
+byse1=byse
+byse=byse/byse
+
+pleiotropy.rm=which(fit.susie$pip>xQTL.pip.thres)
+pleiotropy.keep=setdiff(c(1:m),pleiotropy.rm)
+Theta=matrixInverse(LD)
+yinv=c(matrixVectorMultiply(Theta,by))
+xtx=sum(bXest*bXest0)
+xty=sum(by*bXest0)
+theta.ini=xty/xtx
+Thetarho=matrixInverse(LD[pleiotropy.keep,pleiotropy.keep]+admm.rho*diag(length(pleiotropy.keep)))
+tauvec=sort(tauvec,decreasing=F)
+w=length(tauvec)
+Btheta=c(1:w)
+Bgamma=matrix(0,m,w)
+Bbic=c(1:w)
+for(sss in c(w:1)){
+theta=theta.ini
+gamma=by*0
+gamma1=gamma
+u=admm.rho*(gamma-gamma1)
+theta1=theta*0
+error=1
+iter=1
+fit.theta=NULL
+while(error>max.eps&iter<max.iter){
+indvalid=which(gamma1==0)
+theta1=theta
+if(use.susie==T){
+res=by-matrixVectorMultiply(LD,gamma)
+xtr=sum(bXest0*res)
+rtr=sum(res*(Theta%*%res))
+fit.theta=susie_suff_stat(XtX=as.matrix(xtx),Xty=as.vector(xtr),yty=rtr,L=1,n=m,intercept=F,residual_variance=1,estimate_prior_method="EM",s_init=fit.theta)
+if(fit.theta$pip>=causal.pip.thres){
+Hinv=1/(xtx-sum(bXestse[indvalid]^2)*Rxy[1,1])
+g=sum(bXest0*(by-as.vector(LD%*%gamma)))-sum(bXestse[indvalid])*Rxy[2,1]
+theta=g*Hinv
+}else{
+theta=0
+}
+}else{
+Hinv=1/(xtx-sum(bXestse[indvalid]^2)*Rxy[1,1])
+g=sum(bXest0*(by-as.vector(LD%*%gamma)))-sum(bXestse[indvalid])*Rxy[2,1]
+theta=g*Hinv
+}
+res=c(by-bXest*theta-u+admm.rho*gamma1)
+gamma[pleiotropy.keep]=c(matrixVectorMultiply(Thetarho,res[pleiotropy.keep]))
+gamma1=mcp(gamma+u/admm.rho,tauvec[sss])
+u=u+admm.rho*(gamma-gamma1)
+u[pleiotropy.rm]=0
+iter=iter+1
+if(iter>3){
+error=abs(theta-theta1)
+}
+Btheta[sss]=theta
+Bgamma[,sss]=gamma1
+res=as.vector(by-bXest*theta-matrixVectorMultiply(LD,gamma1))
+df=sum(gamma1!=0)
+rss=sum(res*(matrixVectorMultiply(Theta,res)))
+Bbic[sss]=m*log(rss)+log(m)*(1+ebic.gamma)*df
+}
+}
+
+star=which.min(Bbic)
+theta=Btheta[star]
+gamma=Bgamma[,star]
+indvalid=which(gamma==0)
+indgamma=which(gamma!=0)
+effn=m-length(indgamma)
+
+res=as.vector(by-bXest*theta-LD%*%gamma)
+upsilon=by*0
+var_inf=0
+var_error=1
+if(var.inf==T){
+var_inf=1
+for(vv in 1:10){
+Hupsilon=solve(diag(m)/var_inf+LD)
+upsilon=as.vector(Hupsilon%*%res)
+df=sum(diag(Hupsilon))
+var_inf=min((sum(upsilon^2)+df)/m,10)
+}
+}
+res_inf=res-matrixVectorMultiply(LD,upsilon)
+df=sum(diag(Hupsilon%*%LD))
+var_error=sum(res_inf*(Theta%*%res_inf))/(m-df-1-length(indgamma))
+var_error=max(1,var_error)
+
+if(sum(indgamma)>0){
+Z=cbind(bXest0,diag(m)[,indgamma])
+Hinv=matrixMultiply(t(Z),matrixMultiply(LD,Z))
+Hinv[1,1]=Hinv[1,1]-sum(bXestse[indvalid])*Rxy[2,1]
+Hinv=positiveinv(Hinv)
+Hinv1=matrixListProduct(list(t(Z),Theta,var_error*LD+var_inf*LD%*%LD,Theta,Z))
+COV=Hinv%*%Hinv1%*%Hinv
+covg=COV[1,1]
+covtheta=covg
+}
+
+if(sum(indgamma)==0){
+h0=sum(bXest0*((var_error*LD+var_inf*LD%*%LD)%*%bXest0))
+h1=(xtx-sum(bXestse[indvalid])*Rxy[2,1])
+covtheta=h0/h1/h1
+}
+
+A=list()
+A$theta=theta
+A$gamma=gamma
+A$theta.cov=as.numeric(covtheta)
+A$theta.se=sqrt(A$theta.cov)
+A$theta.z=A$theta/A$theta.se
+A$Bic=Bbic
+A$eQTL.fit=fit.susie
+A$var.error=var_error
+A$var.inf=var_inf
+A$causal.fit=fit.theta
+return(A)
+}
